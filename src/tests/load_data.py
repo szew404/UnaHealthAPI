b7e5421d9fb06ef309df3544307d9e06aeb7b05a
gidaszewski <gidaszewskifranco@gmail.com>
2025-02-12 13:46:27 +0100

    Add static assets including images, fonts, and JavaScript for admin dashboard

---
+++ src/tests/load_data.py
@@ -0,0 +1,21 @@
+# This file works for uploading CSV sample files,
+# this file was only used to test the POST endpoint '/api/v1/upload-csv/'.
+
+import os
+import requests
+
+url = "http://localhost:8000/api/v1/upload-csv/"
+
+sample_data_dir = "src/tests/sample-data"
+
+for filename in os.listdir(sample_data_dir):
+    if filename.endswith(".csv"):
+        file_path = os.path.join(sample_data_dir, filename)
+
+        with open(file_path, "rb") as csv_file:
+            files = {"csv_file": (filename, csv_file, "text/csv")}
+
+            response = requests.post(url, files=files)
+
+            print(f"Response for {filename}: {response.status_code}")
+            print(response.json())
---
+++ src/tests/test_post_glucose.py
@@ -0,0 +1,63 @@
+import pytest
+from rest_framework.test import APIRequestFactory
+from rest_framework import status
+from ..modules.api.views.post_glucose import upload_csv_file
+from django.core.files.uploadedfile import SimpleUploadedFile
+from unittest.mock import patch
+
+
+@pytest.mark.django_db
+class TestUploadCSVFile:
+    def setup_method(self):
+        self.factory = APIRequestFactory()
+        self.url = "/api/upload-csv/"
+
+    def test_upload_csv_file_success(self):
+        csv_content = "date,glucose\n2023-01-01,100\n"
+        csv_file = SimpleUploadedFile(
+            "user1.csv", csv_content.encode("utf-8"), content_type="text/csv"
+        )
+        request = self.factory.post(
+            self.url, {"csv_file": csv_file}, format="multipart"
+        )
+
+        with patch(
+            "modules.tasks.process_csv_file.process_csv_glucose_file.delay"
+        ) as mock_task:
+            response = upload_csv_file(request)
+            assert response.status_code == status.HTTP_202_ACCEPTED
+            assert response.data == {"status": "File is being processed"}
+            mock_task.assert_called_once_with(csv_content=csv_content, user_id="user1")
+
+    def test_upload_csv_file_invalid_serializer(self):
+        request = self.factory.post(self.url, {}, format="multipart")
+        response = upload_csv_file(request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_upload_csv_file_encoding_error(self):
+        csv_file = SimpleUploadedFile(
+            "user1.csv", b"\x80\x81\x82", content_type="text/csv"
+        )
+        request = self.factory.post(
+            self.url, {"csv_file": csv_file}, format="multipart"
+        )
+        response = upload_csv_file(request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data == {"error": "File encoding error"}
+
+    def test_upload_csv_file_general_exception(self):
+        csv_content = "date,glucose\n2023-01-01,100\n"
+        csv_file = SimpleUploadedFile(
+            "user1.csv", csv_content.encode("utf-8"), content_type="text/csv"
+        )
+        request = self.factory.post(
+            self.url, {"csv_file": csv_file}, format="multipart"
+        )
+
+        with patch(
+            "modules.tasks.process_csv_file.process_csv_glucose_file.delay",
+            side_effect=Exception("Test Exception"),
+        ):
+            response = upload_csv_file(request)
+            assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+            assert response.data == {"error": "Test Exception"}
