5c59a754b68d7bfb8cec9dc89cb5349ff1cf028c
gidaszewski <gidaszewskifranco@gmail.com>
2025-02-19 11:11:15 +0100

    Add export endpoint for glucose data in API URLs

--- src/modules/api/urls.py
+++ src/modules/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views.post_glucose import upload_csv_file
 from .views.get_glucose import GlucoseListView, GlucoseDetailView
 from .views.get_task_status import get_task_status
+from .views.export_glucose import ExportGlucoseView
 
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path("levels/", GlucoseListView.as_view(), name="glucose-list"),
     path("levels/<int:pk>/", GlucoseDetailView.as_view(), name="glucose-detail"),
     path("task-status/<str:task_id>/", get_task_status, name="task-status"),
+    path("export-glucose/", ExportGlucoseView.as_view(), name="export-glucose"),
 ]
